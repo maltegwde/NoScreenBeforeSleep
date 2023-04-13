4014ba60b9e727ca27d64d1f7141b97dd79e1b53
Malte Schneider <maltegwde@gmail.com>
2023-04-13 23:52:41 +0200

    removed unused package

--- lib/utils/notification_service.dart
+++ lib/utils/notification_service.dart
@@ -3,7 +3,6 @@ import 'dart:ui';
 
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_native_timezone/flutter_native_timezone.dart';
-import 'package:no_screen_before_sleep/utils/download_util.dart';
 import 'package:rxdart/subjects.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
